632165295977980a00b58e22fabf3ff97e63a8f2
DanKul17 <163841550+DanKul17@users.noreply.github.com>
2024-03-18 13:20:53 +0300

    Add files via upload

---
+++ Код.sh
@@ -0,0 +1,4 @@
+daniil@DESKTOP-AJS4ISA:~$ #!/bin/bash
+daniil@DESKTOP-AJS4ISA:~$ cd /mnt/c/Users/Пк/Desktop
+daniil@DESKTOP-AJS4ISA:/mnt/c/Users/Пк/Desktop$ env > ./Результат_1.txt
+daniil@DESKTOP-AJS4ISA:/mnt/c/Users/Пк/Desktop$ grep "$(whoami)" ./Результат_1.txt > ./Результат_2.txt
